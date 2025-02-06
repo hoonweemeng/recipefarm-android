59081837a51c3d0ae1184f629cdf4ef86c28e049
Hoon Wee Meng <hoonweemeng@gmail.com>
2025-02-07 00:02:57 +0800

    created ui for recipe detail page

---
+++ app/src/main/java/com/app/recipefarm/recipedetail/RecipeDetailActivity.java
@@ -0,0 +1,21 @@
+package com.app.recipefarm.recipedetail;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.app.recipefarm.R;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
